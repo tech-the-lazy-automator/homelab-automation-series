8fb052d36d93b1c57a299023384e8d9bd117fbd1
Tech - The Lazy Automator <techthelazyautomator+github@gmail.com>
2025-09-07 01:53:32 +0530

    Update provider.tf

--- 22-terraform-dynamic-vm-provisioning/provider.tf
+++ 22-terraform-dynamic-vm-provisioning/provider.tf
@@ -21,8 +21,8 @@ variable proxmox_api_token {
 
 provider "proxmox" {
   # Configuration options
-pm_api_url = var.proxmox_api_url
-pm_api_token_id = var.proxmox_api_token_id
-pm_api_token_secret = var.proxmox_api_token
-pm_tls_insecure = true
+  pm_api_url = var.proxmox_api_url
+  pm_api_token_id = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token
+  pm_tls_insecure = true
 }
